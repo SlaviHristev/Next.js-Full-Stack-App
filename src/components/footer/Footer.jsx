b4b86ce20b2879cb83b7d07850ca89e9adcb91e1
SlaviHristev <Slavihristev97@gmail.com>
2024-02-12 18:59:18 +0200

    FInished homepage and footer design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,9 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
-    return <div>
-        Footer
+    return <div className={styles.container}>
+        <div className={styles.logo}>Logo</div>
+        <div className={styles.text}>All rights reserved.</div>
     </div>;
   };
   
