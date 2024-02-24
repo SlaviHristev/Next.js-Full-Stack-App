c4da8b3f142f5981d61f6d61f5d7daf51cd687b9
SlaviHristev <Slavihristev97@gmail.com>
2024-02-24 12:22:48 +0200

    Made models and fetching data endpoints

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,15 +1,49 @@
+import { Post, User } from "./models";
+import { connectDb } from "./utils";
 
 
 
 
-export const getPosts = () =>{
-
+export const getPosts = async () =>{
+    try {
+        connectDb();
+        const posts = await Post.find();
+        return posts;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch posts!')
+    }
 }
 
-export const getPost = () =>{
-
+export const getPost = async (slug) =>{
+    try {
+        connectDb();
+        const post = await Post.find(slug);
+        return post;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch post!')
+    }
 }
 
-export const getPostUser = () =>{
-    
+export const getUser = async (id) =>{
+    try {
+        connectDb();
+        const user = await User.findById(id);
+        return user;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch user!')
+    }
+};
+
+export const getUsers = async () =>{
+    try {
+        connectDb();
+        const users = await User.find();
+        return users;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch users!')
+    }
 }
