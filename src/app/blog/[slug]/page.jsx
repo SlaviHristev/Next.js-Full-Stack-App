50bee02fd20bcc598599a8820dcd94581bd97736
SlaviHristev <Slavihristev97@gmail.com>
2024-02-24 10:55:59 +0200

    Made fetching data for single post page and displaying it

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,28 +1,41 @@
 import Image from 'next/image';
 import styles from './singlePostPage.module.css';
+import PostUser from '@/components/postUser/postUser';
+import { Suspense } from 'react';
 
 
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+  if(!res.ok){
+    throw new Error("Something went wrong!")
+  };
+  return res.json();
+}
 
-const SinglePostPage = () => {
+
+const SinglePostPage = async ({params}) => {
+    const {slug} = params;
+    const post =  await getData(slug);
+
     return <div className={styles.container}>
         <div className={styles.imgContainer}>
           <Image className={styles.img} src='https://images.pexels.com/photos/20056974/pexels-photo-20056974/free-photo-of-fortaleza-brazil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' fill/>
         </div>
         <div className={styles.textContainer}>
-              <h1 className={styles.title}>Title</h1>
+              <h1 className={styles.title}>{post.title}</h1>
               <div className={styles.details}>
                 <Image className={styles.avatar} src='https://images.pexels.com/photos/20056974/pexels-photo-20056974/free-photo-of-fortaleza-brazil.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' width={50} height={50}  />
-                <div className={styles.detailText}>
-                    <span className={styles.detailTitle}>Author</span>
-                    <span className={styles.detailValue}>Slavi Hristev</span>
-                </div>
+                <Suspense fallback={<div>Loading...</div>}>
+
+                <PostUser userId = {post.userId} />
+                </Suspense>
                 <div className={styles.detailText}>
                     <span className={styles.detailTitle}>Published</span>
                     <span className={styles.detailValue}>18.02.2024</span>
                 </div>
               </div>
               <div className={styles.content}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque porro sequi velit at explicabo necessitatibus eos commodi corrupti, repudiandae qui.
+               {post.body}
               </div>
         </div>
     </div>;
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -15,7 +15,7 @@ const PostCard = ({post}) =>{
             <div className={styles.bottom}>
                 <h1 className={styles.title}>{post.title}</h1>
                 <p className={styles.desc}>{post.body}</p>
-                <Link href='/blog/post' className={styles.link}>READ MORE</Link>
+                <Link href={`/blog/${post.id}`} className={styles.link}>READ MORE</Link>
             </div>
         </div>
     )
